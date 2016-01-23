8ed82895f128a9a6dd69e2090129ab494243b092
skipperguy12 <skipperguy12@users.noreply.github.com>
2016-01-23 13:35:43 -0500

    Allow new token creation, prevent tokens for existing users

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -1,5 +1,17 @@
 class TokensController < ApplicationController
   def create
+    matching = Token.where(email: token_params[:email])
+    if matching.exists?
+      matching.each do |d|
+        d.delete
+      end
+    end
+
+    if User.where(email: token_params[:email]).exists?
+      render :json => {error: "A user with that email exists. Are you sure you didn't already register?"}
+      return
+    end
+
     @token = Token.new
     @token.email = token_params[:email]
     @token.token = (0...8).map { (65 + rand(26)).chr }.join.downcase

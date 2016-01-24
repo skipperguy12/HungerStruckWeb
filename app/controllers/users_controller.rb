f5bd88905c7554279220a6ca120d584bbb18a12b
skipperguy12 <skipperguy12@users.noreply.github.com>
2016-01-24 01:49:42 -0500

    Use Crafatar, allow UUIDs for finding users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
   def show
-    @user = User.where(username: params[:id]).first
+    @user = User.where(uuid: params[:id]).first
+    @user = User.where(username: params[:id]).first   if @user.nil?
+    not_found if @user.nil?
+    
     @posts_count = MongoidForums::Post.where(:user_id => @user.id).count
     @topics_count = MongoidForums::Topic.where(:user_id => @user.id, :hidden => false).count
   end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,5 @@
 module UsersHelper
+  def get_image(user, size, style)
+    return image_tag("https://crafatar.com/avatars/" + user.to_s + "?size=#{size}", style: style)
+  end
 end

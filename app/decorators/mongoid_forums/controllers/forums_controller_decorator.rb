d8727bd5dc4b21d4c8c0e7c4dfa678cc4247c450
skipperguy12 <skipperguy12@users.noreply.github.com>
2016-01-30 11:37:57 -0500

    Fix pagination authorization (cancancan) and add it to bottom of my_links

--- app/decorators/mongoid_forums/controllers/forums_controller_decorator.rb
+++ app/decorators/mongoid_forums/controllers/forums_controller_decorator.rb
@@ -2,7 +2,15 @@ MongoidForums::ForumsController.class_eval do
     # Redefinition
     def index
       # start changes
-      @recent = MongoidForums::Topic.by_most_recent_post.page(params[:page]).per(MongoidForums.per_page)
+      @recent = Kaminari.paginate_array(MongoidForums::Topic.by_most_recent_post.select {|t| can? :read, t}).page(params[:page]).per(MongoidForums.per_page)
       # end
+    end
+
+    def show
+      @forum = MongoidForums::Forum.find(params[:id])
+      register_view
+
+      @topics = @forum.topics
+      @topics = Kaminari.paginate_array(@topics.by_pinned_or_most_recent_post.select {|t| can? :read, t}).page(params[:page]).per(MongoidForums.per_page)
     end
 end

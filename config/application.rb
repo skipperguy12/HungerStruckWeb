15d2e7e0ffd7d06f01cbd101474f6e2ccb54920c
skipperguy12 <skipperguy12@users.noreply.github.com>
2016-01-30 18:40:35 -0500

    Give up on Emoji for now

--- config/application.rb
+++ config/application.rb
@@ -18,8 +18,6 @@ require "mongoid_forums"
 
 module HungerStruck
   class Application < Rails::Application
-    config.assets.paths << Emoji.images_path
-    config.assets.precompile << "emoji/**/*.png"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.

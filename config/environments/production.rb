44b397041bd6886f8dc9a11f2ff4a9519e33fe12
skipperguy12 <skipperguy12@users.noreply.github.com>
2016-01-30 02:39:57 -0500

    Update change password form

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,4 +73,19 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { host: 'http://hstrk.xyz' }
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: ENV["GMAIL_DOMAIN"],
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
+
 end
